a77d232d5a4f3d719b1c887b7e768ac4423dbb6a
Srivatsan Varadharajan <varadhsa@miamioh.edu>
2023-07-11 17:32:54 -0400

    rerun VPC. Create ALB

--- main.tf
+++ main.tf
@@ -89,7 +89,7 @@ module "blog_sg" {
   version = "5.1.0"
   name = "blog"
 
-  vpc_id              = module.blog_vpc.public_subnets[0]
+  vpc_id              = module.blog_vpc.vpc_id
   
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
