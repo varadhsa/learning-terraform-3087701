75b447cd0c01e17fee9f2f66ea41d7c246daefe8
Srivatsan Varadharajan <varadhsa@miamioh.edu>
2023-07-19 20:28:49 -0400

    autoscaling

--- main.tf
+++ main.tf
@@ -32,16 +32,7 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  tags = {
-    Name = "HelloWorld"
-  }
-}
 
 module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
@@ -51,6 +42,13 @@ module "autoscaling" {
   name = "blog"
   min_size = 1
   max_size = 2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
 module "blog_alb" {
